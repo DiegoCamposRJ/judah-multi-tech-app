a257bfb6dca5644a7d807f65ca7ab47b926fcd2c
DiegoCamposRJ <diegocamposrj@gmail.com>
2021-07-25 19:28:45 -0300

    resolver problena de conecção entre paginas

---
+++ src/componentes/pages/Servicos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Servicos() {
+    return (
+        <div>
+            <h1>Pagina Serviços</h1>
+        </div>
+    )
+}
+
