87465728039f79adc06281fd4eae7d92ff68c422
DiegoCamposRJ <diegocamposrj@gmail.com>
2021-07-27 19:11:53 -0300

    update imagens e Produção

--- src/componentes/Producao.js
+++ src/componentes/Producao.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Foto1 from './imagens/infra_Judah.jpg';
+import Foto3 from './imagens/live_streaming.jpg';
+import './Producao.css';
 
 function Producao() {
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid prod_container'>
             <div className='row'>
-                <h1>Produção Audiovisual</h1>
-                <p className="">Temos a capacidade em realizar matérias para Live Streamer ou
+                <h1 className='prod_titulo'>Produção Audiovisual</h1>
+                <p className='prod_texto1'>Temos a capacidade em realizar matérias para Live Streamer ou
                     gravação de vídeos de qualquer evento ou aula. Pois, nossa empresa
                     possui os melhores equipamentos a sua disposição.
                 </p>
                 <div className='col-md-6'>
-                    <img className='Foto_1 rounded' src={Foto1} alt="Portfolio"></img>
+                    <img className='Foto_3 rounded' src={Foto3} alt="Portfolio"></img>
                 </div>
                 <div className='col-md-6'>
-                    <h4>
+                    <h4 className='prod_subtitulo'>
                         Produção de conteúdos publicitários,
                         documentarios, jornalísticos e artísticos, o que
                         inclui roteiro, fotografia, iluminação,
@@ -25,7 +26,7 @@ function Producao() {
                 </div>
                 <div className="col-md-5 text-center card">                    
                     <i className='bx bxs-camera-movie card_icon'></i>
-                    <h3 className="card_titulo">Equipamento</h3>
+                    <h3 className="card_titulo3">Equipamento</h3>
                     <p className="card_text">
                         Nossa empresa possui os equipamentos de diferentes
                         orçamentos para sets de gravação
@@ -36,7 +37,7 @@ function Producao() {
                 </div>
                 <div className="col-md-5 text-center card">
                     <i className="fas fa-clipboard-list card_icon"></i>
-                    <h3 className="card_titulo">Pode Contar</h3>
+                    <h3 className="card_titulo3">Pode Contar</h3>
                     <p className="card_text">
                         <ul>
                             <li>Análise Textual</li>
