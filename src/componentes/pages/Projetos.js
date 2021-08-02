62fb4237a9d103fd64d2edfde33fee475fd84c6e
DiegoCamposRJ <diegocamposrj@gmail.com>
2021-08-02 19:31:36 -0300

    Unexpected token

--- src/componentes/pages/Projetos.js
+++ src/componentes/pages/Projetos.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import NossoCliente from '../NossoCliente';
 
 export default function Projetos() {
     return (
         <div>
             <h1>Pagina Projetos</h1>
+            <NossoCliente />
             <Footer/>
         </div>
     )
