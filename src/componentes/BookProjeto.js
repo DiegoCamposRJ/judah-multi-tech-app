497be1a8b23f6f4a1dfb2deef2ec3346dd630e18
DiegoCamposRJ <diegocamposrj@gmail.com>
2021-08-03 19:38:12 -0300

    card book em andamento

--- src/componentes/BookProjeto.js
+++ src/componentes/BookProjeto.js
@@ -1,5 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import {dataProjetos} from './dataProjetos';
+import './BookProjeto.css';
+
 
 export default class BookProjetos extends Component{
     render(){
@@ -10,13 +12,15 @@ export default class BookProjetos extends Component{
                     <div className="row">
                         {
                             dataProjetos.map((item, index) => (
-                                <div className='col-md-3 card' key={item.id}>
-                                    <img alt={item.id} src={item.images} />
-                                    <h3>Projeto: {item.nome}.</h3>
-                                    <h4>Serviço: {item.tipo}</h4>
-                                    <h6>Contratado: {item.ano}</h6>
-                                    <span>{item.status}</span>
-                                    <div className=''>Botão do Link</div>
+                                <div className='col-md-3 cardBook' key={item.id}>
+                                    <img className='' alt={item.id} src={item.images} />
+                                    <div className='card-body'>
+                                        <h3 className="card-title">{item.nome}.</h3>
+                                        <h5>Serviço: {item.tipo}</h5>
+                                        <h6>Contratado: {item.ano}</h6>
+                                        <span className='book_spaw'>{item.status}</span>
+                                        <div className=''>Botão do Link</div>
+                                    </div>
                                 </div>
                             ))
                         }
--- src/componentes/pages/Projetos.js
+++ src/componentes/pages/Projetos.js
@@ -3,13 +3,16 @@ import '../../App.css';
 import Footer from '../Footer';
 import BookProjetos from '../BookProjeto';
 
+
 export default function Projetos() {
     return (
-        <div>
+        <div className='tema_Projetos'>
             <h1>Judah Multi Tech - Projetos</h1>
             <p className='text-center'>Veja agora um pouco de quais instituições a Judah Multi Tech já 
                 realizou serviços sem expor informações confidencial de cada trabalha prestado.
             </p>
+            
+                
             <BookProjetos/>
             <Footer/>
         </div>
